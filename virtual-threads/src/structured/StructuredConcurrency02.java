e2de9260cf7fda75ec0a8371dff6efe80df06520
Trayan Iliev <office@iproduct.org>
2021-05-19 22:10:52 +0300

    use --enable-preview to enable preview APIs

--- virtual-threads/src/structured/StructuredConcurrency02.java
+++ virtual-threads/src/structured/StructuredConcurrency02.java
@@ -2,7 +2,8 @@ package structured;
 
 public class StructuredConcurrency02 {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = Thread.ofVirtual().name("VirtualThread_01").start(() -> System.out.printf("Hello from thread %s%n", Thread.currentThread().getName()));
+        Thread thread = Thread.ofVirtual().name("VirtualThread_01")
+                .start(() -> System.out.printf("Hello from thread %s%n", Thread.currentThread().getName()));
         thread.join();
     }
 }
