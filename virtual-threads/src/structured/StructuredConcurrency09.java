111d91ba183050dbe54b9568dcfd0b4fcdceba5b
Trayan Iliev <office@iproduct.org>
2021-05-20 10:33:23 +0300

    small changes and experiments

--- virtual-threads/src/structured/StructuredConcurrency09.java
+++ virtual-threads/src/structured/StructuredConcurrency09.java
@@ -11,11 +11,17 @@ public class StructuredConcurrency09 {
     public static void main(String[] args) throws InterruptedException {
         try (var e = Executors.newVirtualThreadExecutor()) {
             Stream<Future<String>> tasks = e.submit(List.of(
-                    () -> "a",
                     () -> {
                         throw new IOException("too lazy for work");
                     },
-                    () -> "b"
+                    () -> {
+                        Thread.sleep(10000);
+                        return "a";
+                    },
+                    () -> {
+                        Thread.sleep(1000);
+                        return "b";
+                    }
             ));
 
             String first = tasks
@@ -24,6 +30,7 @@ public class StructuredConcurrency09 {
                     .findFirst()
                     .orElse(null);
             System.out.println("one result: " + first);
+            e.shutdownNow();
         }
 
     }
